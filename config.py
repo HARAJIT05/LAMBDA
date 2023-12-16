48dc89b95399a1c5bedee9c3e68bfa78fc373a8e
HARAJIT <110291590+HARAJIT05@users.noreply.github.com>
2023-12-16 20:43:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
 # config.py
-TOKEN = '6896380150:AAFVVRSHi6CRUaFShL1MGYtnCIUUrI4_wdM'  # Replace with your bot token
-OWNER_USER_ID = 6426928410  # Replace with your user ID
-OPENWEATHERMAP_API_KEY = '8e238375b0ed5d203301c1c6e0ba961f'  # Replace with your OpenWeatherMap API key
-MEME_API_KEY = 'https://meme-api.com/gimme'  # Replace with your Meme API key
-ANILIST_API_URL = 'https://graphql.anilist.co' # AniList GraphQL API URL
-OMDB_API_KEY = "34c4ed25" # OMDB API URL
-OMDB_API_URL = "http://www.omdbapi.com/" # OMDB API URL
 import os
 import time
 import telebot
@@ -23,3 +16,10 @@ import wikipediaapi
 from bs4 import BeautifulSoup
 import speedtest
 from PIL import Image
+TOKEN = 'your_telegram_bot_token'  # Replace with your bot token
+OWNER_USER_ID = your_user_id  # Replace with your user ID
+OPENWEATHERMAP_API_KEY = 'your_openweathermap_api_key'  # Replace with your OpenWeatherMap API key
+MEME_API_KEY = 'https://meme-api.com/gimme'  # Replace with your Meme API key
+ANILIST_API_URL = 'https://graphql.anilist.co'  # AniList GraphQL API URL
+OMDB_API_KEY = 'your_omdb_api_key'  # Replace with your OMDB API key
+OMDB_API_URL = 'http://www.omdbapi.com/'  # OMDB API URL
