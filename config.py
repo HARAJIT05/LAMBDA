202f3178de16f52547d318b4e05a6200cbb41861
HARAJIT <110291590+HARAJIT05@users.noreply.github.com>
2024-08-11 14:08:24 +0000

    hh

--- config.py
+++ config.py
@@ -16,10 +16,10 @@ import wikipediaapi
 from bs4 import BeautifulSoup
 import speedtest
 from PIL import Image
-TOKEN = 'your_telegram_bot_token'  # Replace with your bot token
-OWNER_USER_ID = your_user_id  # Replace with your user ID
-OPENWEATHERMAP_API_KEY = 'your_openweathermap_api_key'  # Replace with your OpenWeatherMap API key
+TOKEN = '5776066556:AAE8uLvD0a7vhbiKHyVtlGU2QXe17abNkoo'  # Replace with your bot token
+OWNER_USER_ID = 6426928410  # Replace with your user ID
+OPENWEATHERMAP_API_KEY = '3865822f731ebc30e889367ce489e234'  # Replace with your OpenWeatherMap API key
 MEME_API_KEY = 'https://meme-api.com/gimme'  # Replace with your Meme API key
 ANILIST_API_URL = 'https://graphql.anilist.co'  # AniList GraphQL API URL
-OMDB_API_KEY = 'your_omdb_api_key'  # Replace with your OMDB API key
+OMDB_API_KEY = 'http://www.omdbapi.com/?i=tt3896198&apikey=f9802666'  # Replace with your OMDB API key
 OMDB_API_URL = 'http://www.omdbapi.com/'  # OMDB API URL
